ba76aca4b0646ad656a3f4fea6f7666d6bcfb2ab
ebrahim-bakhtyarzadeh <ebrahimbakhtyarzadeh@gmail.com>
2024-05-06 22:16:54 +0330

    Add project files.

---
+++ Shop/Shop.Domain/UserAgg/Users.cs
@@ -0,0 +1,20 @@
+﻿using Common.Domain;
+using Shop.Domain.UserAgg.Enums;
+
+namespace Shop.Domain.UserAgg
+{
+    public class Users : AggregateRoot
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public Gender Gender { get; private set; }
+
+        public List<UserRole> Roles { get; private set; }
+        public List<UserAddress> Addresses { get; private set; }
+        public List<Wallet> Wallets { get; private set; }
+
+    }
+}
---
+++ Shop/Shop.Domain/UserAgg/Wallet.cs
@@ -0,0 +1,8 @@
+﻿using Common.Domain;
+
+namespace Shop.Domain.UserAgg;
+
+public class Wallet : BaseEntity
+{
+
+}
