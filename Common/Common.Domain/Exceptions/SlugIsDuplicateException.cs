c8e8dbd29acc1d8df955db4b557d4e726decd0fc
ebrahim-bakhtyarzadeh <ebrahimbakhtyarzadeh@gmail.com>
2024-05-08 01:00:56 +0330

    Complete ProductAgg Task

---
+++ Common/Common.Domain/Exceptions/SlugIsDuplicateException.cs
@@ -0,0 +1,13 @@
+﻿namespace Common.Domain.Exceptions;
+
+public class SlugIsDuplicateException : BaseDomainException
+{
+    public SlugIsDuplicateException() : base("Slug Is Duplicate")
+    {
+
+    }
+    public SlugIsDuplicateException(string message) : base(message)
+    {
+
+    }
+}
