076fa6aa975b5e10236462fcc16d9a7ed51907c7
ebrahim-bakhtyarzadeh <ebrahimbakhtyarzadeh@gmail.com>
2024-08-09 18:13:48 +0330

    Customize BadRequest Response

--- Common/Common.Application/Validation/ValidationMessages.cs
+++ Common/Common.Application/Validation/ValidationMessages.cs
@@ -4,7 +4,7 @@
     {
         public const string Required = "وارد کردن این فیلد اجباری است";
 
-        public const string InvalidPhoneNumber = "Phone Number Is not Valid";
+        public const string InvalidPhoneNumber = "شماره تماس معتبر نیست";
         public const string NotFound = "اطلاعات درخواستی یافت نشد";
         public const string MaxLength = "تعداد کاراکتر های وارد شده بیشتر از حد مجاز است";
         public const string MinLength = "تعداد کاراکتر های وارد شده کمتر از حد مجاز است";
--- Shop/Shop.Api/Program.cs
+++ Shop/Shop.Api/Program.cs
@@ -1,7 +1,9 @@
+using AspNetCore;
 using AspNetCore.Middlewares;
 using Common.Application;
 using Common.Application.FileUtil.Interfaces;
 using Common.Application.FileUtil.Services;
+using Microsoft.AspNetCore.Mvc;
 using Shop.Api.Infrastructure.JwtUtil;
 using Shop.Config;
 
@@ -9,7 +11,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(option =>
+    {
+        option.InvalidModelStateResponseFactory = (context =>
+        {
+
+            var result = new ApiResult()
+            {
+                IsSuccess = false,
+                MetaData = new()
+                {
+                    AppStatusCode = AppStatusCode.BadRequest,
+                    Message = ModelStateUtil.GetModelStateErrors(context.ModelState)
+                }
+            };
+
+            return new BadRequestObjectResult(result);
+        });
+    });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
